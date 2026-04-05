016b51e89c2523f4d415a279ffc743a2c47e70bc
Jeckson77 <gerhardnaftal755@gmail.com>
2026-04-05 23:39:08 +0200

    Configure GitHub Pages deployment via GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Namibia-Coastal-FPU-Marine-SMR-Platform/',
 })
